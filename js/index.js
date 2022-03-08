1b71b741872ebf8788e2657faeac8b870bb1b103
areumsheep <areumsheep@gmail.com>
2022-03-08 22:44:46 +0900

    🔥 index.js 테스트 코드 제거

--- js/index.js
+++ js/index.js
@@ -3,14 +3,3 @@ import { getRegul } from './regulation.js';
 import { getNegotiation } from './negotiation.js';
 import { getDamageList } from './category_damage.js';
 import { search } from './search.js';
-
-const clickDamageBtn = document
-  .getElementsByClassName('damage-btn')[0]
-  .addEventListener('click', () => {
-    console.log('hi');
-  });
-
-const init = funtion(() => {
-  clickDamageBtn();
-});
-init();
