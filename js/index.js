07afd0a2a8068eb7e106bbddc39c2189b1472753
areumsheep <areumsheep@gmail.com>
2022-03-08 22:41:33 +0900

    ✨ 피해 상황 렌더링 구현

--- js/index.js
+++ js/index.js
@@ -1,4 +1,16 @@
 import { getLocalLive, removeData } from './locallive.js';
 import { getRegul } from './regulation.js';
 import { getNegotiation } from './negotiation.js';
+import { getDamageList } from './category_damage.js';
 import { search } from './search.js';
+
+const clickDamageBtn = document
+  .getElementsByClassName('damage-btn')[0]
+  .addEventListener('click', () => {
+    console.log('hi');
+  });
+
+const init = funtion(() => {
+  clickDamageBtn();
+});
+init();
