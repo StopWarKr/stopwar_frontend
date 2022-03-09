eb9b953b9c4cf52e3e8e5a2eec30bc66c7fc74d8
chango <kimtothechang@gmail.com>
2022-03-09 14:58:17 +0900

    ♻️  상수 및 변수 분리

---
+++ js/common/constants.js
@@ -0,0 +1,13 @@
+const BASE_URL = 'https://stopwarkr.github.io/stopwar_frontend/';
+
+const AllCategory = {
+  역사: 'kr_HistoryNewsData.json',
+  규제: 'kr_RegulationNewsData.json',
+  '협상 진행 과정': 'kr_NegoNewsData.json',
+  '지역 라이브 상황': 'kr_LocalNewsData.json',
+  '피해 상황': 'kr_DamageNewsData.json',
+  '교전 최신 상황': 'kr_BattleNewsData.json',
+  후원: 'kr_SponsorNewsData.json',
+};
+
+export { BASE_URL, AllCategory };
