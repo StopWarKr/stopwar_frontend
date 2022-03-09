cfe97a8abafaeb2efdbc77cf2e321f07d9dc25bd
chango <kimtothechang@gmail.com>
2022-03-09 13:09:28 +0900

    🧪  QA 테스트용

--- js/index.js
+++ js/index.js
@@ -3,4 +3,6 @@ import { getRegul } from './regulation.js';
 import { getNegotiation } from './negotiation.js';
 import { getDamageList } from './category_damage.js';
 import { search } from './search.js';
-import { openModal, closeModal } from './modal.js';
+// import { openModal, closeModal } from './modal.js';
+
+search();
